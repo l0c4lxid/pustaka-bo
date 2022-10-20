c62c8f13c2256cf0001c8fdf178d1208e877af46
l0c4lxid <syaidxandhika@gmail.com>
2022-10-20 14:07:27 +0700

    add pertemuan yang kurang

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+	public function index()
+	{
+		echo "<h1>Perkenalkan</h1>";
+		echo"Nama saya Syaid Andhika
+		Saya tingga di daerah Karet
+		olah raga yang saya sukai adalah Bulutangkis";
+ }
+}
